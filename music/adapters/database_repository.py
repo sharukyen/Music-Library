49c0db412128c512ff9b4acf4a5f2325b3c958a9
Stanley W <stanleywu90@gmail.com>
2022-10-10 23:00:18 +1300

    Update database_repository.py

--- music/adapters/database_repository.py
+++ music/adapters/database_repository.py
@@ -1,5 +1,6 @@
 from datetime import date
 from typing import List
+from pathlib import Path
 
 from sqlalchemy import desc, asc
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
@@ -10,9 +11,11 @@ from music.domainmodel.track import Track
 from music.domainmodel.user import User
 from music.domainmodel.review import Review
 from music.domainmodel.genre import Genre
-
+from music.domainmodel.artist import Artist
+from music.domainmodel.album import Album
 
 from music.adapters.repository import AbstractRepository
+from music.adapters.csvdatareader import TrackCSVReader
 
 
 class SessionContextManager:
@@ -62,7 +65,7 @@ class SqlAlchemyRepository(AbstractRepository):
 
     def add_user(self, user: User):
         with self._session_cm as scm:
-            scm.session.add(user)
+            scm.session.merge(user)
             scm.commit()
 
     def get_user(self, user_name: str) -> User:
@@ -77,20 +80,26 @@ class SqlAlchemyRepository(AbstractRepository):
 
     def add_tracks(self, track: Track):
         with self._session_cm as scm:
-            #scm.session.add(track) use merge instead of add
+            # scm.session.add(track) use merge instead of add
             scm.session.merge(track)
+            scm.commit()
+
+    def add_albums(self, album: Album):
+        with self._session_cm as scm:
+            # scm.session.add(track) use merge instead of add
+            scm.session.merge(album)
             scm.commit()
 
     def add_artists(self, artist: Artist):
         with self._session_cm as scm:
-            #scm.session.add(track) use merge instead of add
+            # scm.session.add(track) use merge instead of add
             scm.session.merge(artist)
             scm.commit()
 
-    def get_track(self, id: int) -> Track:
+    def get_track(self, id: int, ) -> Track:
         track = None
         try:
-            track = self._session_cm.session.query(Track).filter(Track._Track__id == id).one()
+            track = self._session_cm.session.query(Track).filter(Track.track_id == id).one()
         except NoResultFound:
             # Ignore any exception and return None.
             pass
@@ -102,8 +111,8 @@ class SqlAlchemyRepository(AbstractRepository):
         return track
 
     def get_tracks_by_range(self, range):
-        existing_ids = [id for id in range if id in self.__tracks_index]
-        tracks = [self.__tracks_index[id] for id in existing_ids]
+        existing_ids = [id for id in range if id in self._session_cm.session.query(Track)]
+        tracks = [self._session_cm.session.query(Track).filter(Track.track_id == id) for id in existing_ids]
         return tracks
 
     def get_number_of_tracks(self):
@@ -115,18 +124,19 @@ class SqlAlchemyRepository(AbstractRepository):
         return tracks
 
     def get_last_track(self):
-        tracks = self._session_cm.session.query(Track).order_by(desc(Track._Article__id)).first()
+        tracks = self._session_cm.session.query(Track).order_by(desc(Track.track_id)).first()
         return tracks
 
     def get_tracks_by_id(self, id_list: List[int]):
-        tracks = self._session_cm.session.query(Track).filter(Track._Article__id.in_(id_list)).all()
+        tracks = self._session_cm.session.query(Track).filter(Track.track_id.in_(id_list)).all()
         return tracks
 
     def get_track_ids_for_genre(self, genre_name: str):
         track_ids = []
 
         # Use native SQL to retrieve article ids, since there is no mapped class for the article_tags table.
-        row = self._session_cm.session.execute('SELECT id FROM genres WHERE genre_name = :genre_name', {'genre_name': genre_name}).fetchone()
+        row = self._session_cm.session.execute('SELECT id FROM genres WHERE genre_name = :genre_name',
+                                               {'genre_name': genre_name}).fetchone()
 
         if row is None:
             # No tag with the name tag_name - create an empty list.
@@ -135,8 +145,8 @@ class SqlAlchemyRepository(AbstractRepository):
             genre_id = row[0]
             # Retrieve article ids of articles associated with the tag.
             track_ids = self._session_cm.session.execute(
-                    'SELECT track_id FROM track_genres WHERE genre_id = :genre_id ORDER BY track_ids ASC',
-                    {'genre_id': genre_id}
+                'SELECT track_id FROM track_genres WHERE genre_id = :genre_id ORDER BY track_ids ASC',
+                {'genre_id': genre_id}
             ).fetchall()
             track_ids = [id[0] for id in track_ids]
 
@@ -146,12 +156,26 @@ class SqlAlchemyRepository(AbstractRepository):
         tags = self._session_cm.session.query(Genre).all()
         return tags
 
-    def add_genres(self, tag: Genre):
+    def add_genres(self, genre: Genre):
         with self._session_cm as scm:
-            scm.session.add(tag)
+            scm.session.merge(genre)
             scm.commit()
 
-    def get_reviews(self) -> List[Review]:
-        reviews = self._session_cm.session.query(Review).all()
-        return reviews
+
+def populate(data_path: Path, repo: SqlAlchemyRepository, database_made: bool):
+    load = TrackCSVReader(str(data_path) + "/raw_albums_excerpt.csv", str(data_path) + "/raw_tracks_excerpt.csv")
+
+    load.read_csv_files()
+
+    for track in load.dataset_of_tracks:
+        repo.add_tracks(track)
+
+    for album in load.dataset_of_albums:
+        repo.add_albums(album)
+
+    for genre in load.dataset_of_genres:
+        repo.add_genres(genre)
+
+    for artist in load.dataset_of_artists:
+        repo.add_artists(artist)
 
